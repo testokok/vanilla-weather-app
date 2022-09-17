582ce7355cc0a449edadb3ad62eaa7f044cb01e6
testokok <nadiroksana@gmail.com>
2022-09-16 22:12:33 -0400

    update time in js

--- src/app.js
+++ src/app.js
@@ -1,12 +1,20 @@
 function formatDate(timestamp) {
     let date = new Date(timestamp);
-    let day = date.getDay();
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    let day = days[date.getDay()];
     let hours = date.getHours();
+    if (hours < 10) {
+        hours = `0${hours}`;
+    }
     let minutes = date.getMinutes();
-    let month = date.getMonth();
+    if (minutes < 10) {
+        minutes = `0${minutes}`;
+    }
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    let month = months[date.getMonth()];
     let datenumber = date.getDate();
     let year = date.getFullYear();
-    return `${day} ${hours}:${minutes}  ${month} ${datenumber}, ${year}`;
+    return `${day} ${hours}:${minutes} ${month} ${datenumber}, ${year}`;
 }
 
 
