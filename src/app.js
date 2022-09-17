15c63f9d44f030e99cc834cd618e8b4caa2e52c3
testokok <nadiroksana@gmail.com>
2022-09-16 21:29:15 -0400

    added code to js file

--- src/app.js
+++ src/app.js
@@ -1,10 +1,14 @@
 function showTemperature(response) {
     let temperatureElement = document.querySelector("#temp-unit");
-    temperatureElement.innerHTML = Math.round(response.data.main.temp);
     let cityElement = document.querySelector("#city-name");
-    cityElement.innerHTML = response.data.name;
     let descriptionElement = document.querySelector("#description");
+    let humidityElement = document.querySelector("#humidity");
+    let windElement = document.querySelector("#wind");
+    temperatureElement.innerHTML = Math.round(response.data.main.temp);
+    cityElement.innerHTML = response.data.name;
     descriptionElement.innerHTML = response.data.weather[0].description;
+    humidityElement.innerHTML = response.data.main.humidity;
+    windElement.innerHTML = response.data.wind.speed;
 }
 
 let apiKey = "502dc8f7ae36e57af1974e18d16a86f8";
