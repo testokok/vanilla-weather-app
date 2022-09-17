55c4c60985b7eed1374963e62e55f29901ca9e3f
testokok <nadiroksana@gmail.com>
2022-09-16 21:53:44 -0400

    added js code

--- src/app.js
+++ src/app.js
@@ -1,14 +1,28 @@
+function formatDate(timestamp) {
+    let date = new Date(timestamp);
+    let day = date.getDay();
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let month = date.getMonth();
+    let datenumber = date.getDate();
+    let year = date.getFullYear();
+    return `${day} ${hours}:${minutes}  ${month} ${datenumber}, ${year}`;
+}
+
+
 function showTemperature(response) {
     let temperatureElement = document.querySelector("#temp-unit");
     let cityElement = document.querySelector("#city-name");
     let descriptionElement = document.querySelector("#description");
     let humidityElement = document.querySelector("#humidity");
     let windElement = document.querySelector("#wind");
+    let dayTimeElement = document.querySelector("#dateTime");
     temperatureElement.innerHTML = Math.round(response.data.main.temp);
     cityElement.innerHTML = response.data.name;
     descriptionElement.innerHTML = response.data.weather[0].description;
     humidityElement.innerHTML = response.data.main.humidity;
-    windElement.innerHTML = response.data.wind.speed;
+    windElement.innerHTML = Math.round(response.data.wind.speed);
+    dateTimeElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 let apiKey = "502dc8f7ae36e57af1974e18d16a86f8";
