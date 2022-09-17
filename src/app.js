98afd6ba22a007a36c777ccc0d27848f5ec429e1
testokok <nadiroksana@gmail.com>
2022-09-16 21:20:34 -0400

    update app.js

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ function showTemperature(response) {
     temperatureElement.innerHTML = Math.round(response.data.main.temp);
     let cityElement = document.querySelector("#city-name");
     cityElement.innerHTML = response.data.name;
+    let descriptionElement = document.querySelector("#description");
+    descriptionElement.innerHTML = response.data.weather[0].description;
 }
 
 let apiKey = "502dc8f7ae36e57af1974e18d16a86f8";
