49143789bbf9266a202146d2a3e89e4227893b10
testokok <nadiroksana@gmail.com>
2022-09-24 22:38:00 -0400

    removed console commands

--- src/app.js
+++ src/app.js
@@ -55,10 +55,8 @@ function displayForecast (response) {
 }
 
 function getForecast(coordinates) {
-    console.log(coordinates);
     let apiKey = "eae061c95483dd066657bfc7525418ed";
     let apiURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
-    console.log(apiURL);
     axios.get(apiURL).then(displayForecast);
 }
 
